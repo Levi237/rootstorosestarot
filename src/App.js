0618f146ae98539797395e3c2da70443bd55da66
Levi Winans <levieiko@gmail.com>
2021-01-03 21:23:09 -0800

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
-import * as routes from 'constan/routes';
+import * as routes from './constants/routes';
 import { Switch, Router } from 'react-router-dom';
-
+import styled from 'styled-components';
 export default class App extends Component {
 render(){
 
 
   return (
     <AppContainer>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </AppContainer>
   );
-}
-}
+};
+};
 
 const AppContainer = styled.div`
   width: 100vw;
 `;
-
