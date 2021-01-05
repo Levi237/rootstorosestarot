ab90ca9fd4a0ad230f1e3ed2d1ef754275d2bad0
Levi Winans <levieiko@gmail.com>
2021-01-05 13:28:49 -0800

    style dealt deck

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -1,397 +1,242 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 export default class Deck extends Component {
     state = {
         deck: [
             {
                 number: "00",
-                image: "./deck/00.jpg",
-                
                 title: "the fool",
             },{
                 number: "01",
-                image: "./deck/01.jpg",
-                
                 title: "the magician",
             },{
                 number: "02",
-                image: "./deck/02.jpg",
-                
                 title: "the high priestess",
             },{
                 number: "03",
-                image: "./deck/03.jpg",
-                
                 title: "the empress",
             },{
                 number: "04",
-                image: "./deck/04.jpg",
-                
                 title: "the emporer",
             },{
                 number: "05",
-                image: "./deck/05.jpg",
-                
                 title: "the heirophant",
             },{
                 number: "06",
-                image: "./deck/06.jpg",
-                
                 title: "the lovers",
             },{
                 number: "07",
-                image: "./deck/07.jpg",
-                
                 title: "the chariot",
             },{
                 number: "08",
-                image: "./deck/08.jpg",
-                
                 title: "strength",
             },{
                 number: "09",
-                image: "./deck/09.jpg",
-                
                 title: "the hermit",
             },{
                 number: "10",
-                image: "./deck/10.jpg",
-                
                 title: "wheel of fortune",
             },{
                 number: "11",
-                image: "./deck/11.jpg",
-                
                 title: "justice",
             },{
                 number: "12",
-                image: "./deck/12.jpg",
-                
                 title: "the hanged man",
             },{
                 number: "13",
-                image: "./deck/13.jpg",
-                
                 title: "death",
             },{
                 number: "14",
-                image: "./deck/14.jpg",
-                
                 title: "temperance",
             },{
                 number: "15",
-                image: "./deck/15.jpg",
-                
                 title: "the devil",
             },{
                 number: "16",
-                image: "./deck/16.jpg",
-                
                 title: "the tower",
             },{
                 number: "17",
-                image: "./deck/17.jpg",
-                
                 title: "the star",
             },{
                 number: "18",
-                image: "./deck/18.jpg",
-                
                 title: "the moon",
             },{
                 number: "19",
-                image: "./deck/19.jpg",
-                
                 title: "the sun",
             },{
                 number: "20",
-                image: "./deck/20.jpg",
-                
                 title: "judgement",
             },{
                 number: "21",
-                image: "./deck/21.jpg",
-                
                 title: "the world",
             },{
                 number: "22",
-                image: "./deck/22.jpg",
-                
                 title: "ace of wands",
             },{
                 number: "23",
-                image: "./deck/23.jpg",
-                
                 title: "two of wands",
             },{
                 number: "24",
-                image: "./deck/24.jpg",
-                
                 title: "three of wands",
             },{
                 number: "25",
-                image: "./deck/25.jpg",
-                
                 title: "four of wands",
             },{
                 number: "26",
-                image: "./deck/26.jpg",
-                
                 title: "five of wands",
             },{
                 number: "27",
-                image: "./deck/27.jpg",
-                
                 title: "six of wands",
             },{
                 number: "28",
-                image: "./deck/28.jpg",
-                
                 title: "seven of wands",
             },{
                 number: "29",
-                image: "./deck/29.jpg",
-                
                 title: "eight of wands",
             },{
                 number: "30",
-                image: "./deck/30.jpg",
-                
                 title: "nine of wands",
             },{
                 number: "31",
-                image: "./deck/31.jpg",
-                
                 title: "ten of wands",
             },{
                 number: "32",
-                image: "./deck/32.jpg",
-                
                 title: "page of wands",
             },{
                 number: "33",
-                image: "./deck/33.jpg",
-                
                 title: "knight of wands",
             },{
                 number: "34",
-                image: "./deck/34.jpg",
-                
                 title: "queen of wands",
             },{
                 number: "35",
-                image: "./deck/35.jpg",
-                
                 title: "king of wands",
             },{
                 number: "36",
-                image: "./deck/36.jpg",
-                
                 title: "ace of cups",
             },{
                 number: "37",
-                image: "./deck/37.jpg",
-                
                 title: "two of cups",
             },{
                 number: "38",
-                image: "./deck/38.jpg",
-                
                 title: "three of cups",
             },{
                 number: "39",
-                image: "./deck/39.jpg",
-                
                 title: "four of cups",
             },{
                 number: "40",
-                image: "./deck/40.jpg",
-                
                 title: "five of cups",
             },{
                 number: "41",
-                image: "./deck/41.jpg",
-                
                 title: "six of cups",
             },{
                 number: "42",
-                image: "./deck/42.jpg",
-                
                 title: "seven of cups",
             },{
                 number: "43",
-                image: "./deck/43.jpg",
-                
                 title: "eight of cups",
             },{
                 number: "44",
-                image: "./deck/44.jpg",
-                
                 title: "nine of cups",
             },{
                 number: "45",
-                image: "./deck/45.jpg",
-                
                 title: "ten of cups",
             },{
                 number: "46",
-                image: "./deck/46.jpg",
-                
                 title: "page of cups",
             },{
                 number: "47",
-                image: "./deck/47.jpg",
-                
                 title: "knight of cups",
             },{
                 number: "48",
-                image: "./deck/48.jpg",
-                
                 title: "queen of cups",
             },{
                 number: "49",
-                image: "./deck/49.jpg",
-                
                 title: "king of cups",
             },{
                 number: "50",
-                image: "./deck/50.jpg",
-                
                 title: "ace of swords",
             },{
                 number: "51",
-                image: "./deck/51.jpg",
-                
                 title: "two of swords",
             },{
                 number: "52",
-                image: "./deck/52.jpg",
-                
                 title: "three of swords",
             },{
                 number: "53",
-                image: "./deck/53.jpg",
-                
                 title: "four of swords",
             },{
                 number: "54",
-                image: "./deck/54.jpg",
-                
                 title: "five of swords",
             },{
                 number: "55",
-                image: "./deck/55.jpg",
-                
                 title: "six of swords",
             },{
                 number: "56",
-                image: "./deck/56.jpg",
-                
                 title: "seven of swords",
             },{
                 number: "57",
-                image: "./deck/57.jpg",
-                
                 title: "eight of swords",
             },{
                 number: "58",
-                image: "./deck/58.jpg",
-                
                 title: "nine of swords",
             },{
                 number: "59",
-                image: "./deck/59.jpg",
-                
                 title: "ten of swords",
             },{
                 number: "60",
-                image: "./deck/60.jpg",
-                
                 title: "page of swords",
             },{
                 number: "61",
-                image: "./deck/61.jpg",
-                
                 title: "knight of swords",
             },{
                 number: "62",
-                image: "./deck/62.jpg",
-                
                 title: "queen of swords",
             },{
                 number: "63",
-                image: "./deck/63.jpg",
-                
                 title: "king of swords",
             },{
                 number: "64",
-                image: "./deck/64.jpg",
-                
                 title: "ace of pentacles",
             },{
                 number: "65",
-                image: "./deck/65.jpg",
-                
                 title: "two of pentacles",
             },{
                 number: "66",
-                image: "./deck/66.jpg",
-                
                 title: "three of pentacles",
             },{
                 number: "67",
-                image: "./deck/67.jpg",
-                
                 title: "four of pentacles",
             },{
                 number: "68",
-                image: "./deck/68.jpg",
-                
                 title: "five of pentacles",
             },{
                 number: "69",
-                image: "./deck/69.jpg",
-                
                 title: "six of pentacles",
             },{
                 number: "70",
-                image: "./deck/70.jpg",
-                
                 title: "seven of pentacles",
             },{
                 number: "71",
-                image: "./deck/71.jpg",
-                
                 title: "eight of pentacles",
             },{
                 number: "72",
-                image: "./deck/72.jpg",
-                
                 title: "nine of pentacles",
             },{
                 number: "73",
-                image: "./deck/73.jpg",
-                
                 title: "ten of pentacles",
             },{
                 number: "74",
-                image: "./deck/74.jpg",
-                
                 title: "page of pentacles",
             },{
                 number: "75",
-                image: "./deck/75.jpg",
-                
                 title: "knight of pentacles",
             },{
                 number: "76",
-                image: "./deck/76.jpg",
-                
                 title: "queen of pentacles",
             },{
                 number: "77",
-                image: "./deck/77.jpg",
-                
                 title: "king of pentacles",
             }
         ]
@@ -425,7 +270,7 @@ export default class Deck extends Component {
         const dealCards = () => {
             const container = document.getElementById('deckDisplay');
             shuffledDeck.map((card) => {
-                container.innerHTML += `<div id="${card.number}"><img src="./deck/back.jpg"></div>`;
+                container.innerHTML += `<div id="${card.number}" class="dealtCard"><img src="./deck/tab/back.jpg"></div>`;
             });
         };
         
@@ -442,7 +287,7 @@ export default class Deck extends Component {
             reshuffleBtn.style.display = "none";
         };
         return (
-            <>
+            <Container>
                 <h1>DECK DISPLAY</h1>
                 <button id="shuffle" onClick={shuffleCards}>
                     Shuffle
@@ -456,11 +301,30 @@ export default class Deck extends Component {
                 <button id="clear" onClick={clearDeck}>
                     Clear
                 </button>
-                <div id="deckDisplay">
-                    
+                <div>
+
+                    <div id="deckDisplay">
+                        
+                    </div>
                 </div>
 
-            </>
+            </Container>
         );
     };
 };
+
+const Container = styled.div`
+    width: 100vw;
+    > div:first-of-type {
+        width: 100vw;
+        overflow: scroll;
+
+        div#deckDisplay {
+            width: 3978px;
+            height: 280px;
+            padding-top: 50px;
+            overflow: scroll;
+          }
+    }
+
+`;
