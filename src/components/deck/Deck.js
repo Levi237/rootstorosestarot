3c1e0f9ca2f697ac8e4975fbe34afe8398cb518e
Levi Winans <levieiko@gmail.com>
2021-01-04 17:35:49 -0800

    set up deck gen

---
+++ src/components/deck/Deck.js
@@ -0,0 +1,22 @@
+// const shuffledDeck = [];
+
+// const shuffleCards = ()  => {
+//     while (deck.length > 0) {
+//         let index = Math.floor(Math.random() * deck.length);
+//         let card = deck[index];
+//         shuffledDeck.push(card);
+//         deck.splice(index, 1);
+//     }
+// }
+
+// Create Deck Object Array
+
+import React, { Component } from 'react';
+export default class Deck extends Component {
+    state = {
+
+    }
+    render(){
+        return (<></>);
+    };
+};
