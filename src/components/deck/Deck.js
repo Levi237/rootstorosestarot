b299b17e8d56635ad84f161694a8ed7a63243b72
Levi Winans <levieiko@gmail.com>
2021-01-04 20:39:03 -0800

    shuffle deck multiple times

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -1,62 +1,85 @@
-
-
-// Create Deck Object Array
-
 import React, { Component } from 'react';
 
 export default class Deck extends Component {
     state = {
-        deck: [{
-            cardOne: {
+        deck: [
+            {
                 title: "card one",
                 image: "😬",
                 info: "one blah blah blah blah blah blah blah blah"
-            },
-            cardTwo: {
+            },{
                 title: "card two",
                 image: "😊",
                 info: "two blah blah blah blah blah blah blah blah"
-            },
-            cardThree: {
+            },{
                 title: "card three",
                 image: "😎",
                 info: "three blah blah blah blah blah blah blah blah"
-            },
-            cardFour: {
+            },{
                 title: "card four",
                 image: "🤗",
                 info: "four blah blah blah blah blah blah blah blah"
             }
-        }]
+            ]
     };
     render(){
         const { deck } = this.state;
-        const shuffledDeck = [];
+        let newDeck = [...deck];
+        let shuffledDeck = [];
 
-        const shuffleCards = ()  => {
-            while (deck.length > 0) {
-                let index = Math.floor(Math.random() * deck.length);
-                let card = deck[index];
+        const shuffleCards = () => {
+            while (newDeck.length > 0) {
+                let index = Math.floor(Math.random() * newDeck.length);
+                let card = newDeck[index];
                 shuffledDeck.push(card);
-                deck.splice(index, 1);
+                newDeck.splice(index, 1);
             };
+            console.log(shuffledDeck, "shuffled cards")
         };
-        const dealCards = () => {
-            let container = document.getElementById('deckDisplay')
-            container.innerHTML = `<div><h2>${shuffledDeck[0].cardOne.title}</h2></div>`
-        }
-        const shuffleDealCards = () => {
+        const shuffleAgain = () => {
+            newDeck = [...deck];
+            clearDeck();
             shuffleCards();
-            dealCards();
+            console.log(shuffledDeck, "shuffled again")
+        };
+        
+        const dealCards = () => {
+            const container = document.getElementById('deckDisplay');
+            console.log(container, "container")
+            container.innerHTML = `<div><h2>test</h2></div>`;
+            console.log(shuffledDeck, "deal deck", deck, "<--- deck")
         }
+        // const shuffleDealCards = () => {
+        //     shuffleCards();
+        //     dealCards();
+        //     // console.log(shuffledDeck, "shuffledDeck");
+        // };
+        const clearDeck = () => {
+            // deck = shuffledDeck;
+            const container = document.getElementById('deckDisplay');
+            while (shuffledDeck.length > 0) {
+                shuffledDeck = [];
+                container.innerHTML = `<div><h2>empty</h2></div>`;
+            }
+            console.log(shuffledDeck, "clearDeck");
+        };
         return (
             <>
                 <h1>DECK DISPLAY</h1>
                 <div id="deckDisplay">
-                    
+                    <div><h2>landing</h2></div>
                 </div>
-                <button onClick={shuffleDealCards}>
+                <button onClick={shuffleCards}>
+                    Shuffle Deck
+                </button>
+                <button onClick={dealCards}>
                     Deal Deck
+                </button>
+                <button onClick={shuffleAgain}>
+                    shuffleAgain
+                </button>
+                <button onClick={clearDeck}>
+                    Clear
                 </button>
             </>
         );
