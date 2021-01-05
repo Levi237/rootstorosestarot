a6e2c03a9504db495b3911340f4a466aef28ad00
Levi Winans <levieiko@gmail.com>
2021-01-04 17:46:47 -0800

    start shuffle with simple state

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -1,22 +1,50 @@
-// const shuffledDeck = [];
 
-// const shuffleCards = ()  => {
-//     while (deck.length > 0) {
-//         let index = Math.floor(Math.random() * deck.length);
-//         let card = deck[index];
-//         shuffledDeck.push(card);
-//         deck.splice(index, 1);
-//     }
-// }
 
 // Create Deck Object Array
 
 import React, { Component } from 'react';
 export default class Deck extends Component {
     state = {
-
+        deck: {
+            cardOne: {
+                title: "card one",
+                image: "😬",
+                info: "blah blah blah blah blah blah blah blah blah"
+            },
+            cardTwo: {
+                title: "card one",
+                image: "😊",
+                info: "blah blah blah blah blah blah blah blah blah"
+            },
+            cardThree: {
+                title: "card one",
+                image: "😎",
+                info: "blah blah blah blah blah blah blah blah blah"
+            },
+            cardFour: {
+                title: "card one",
+                image: "🤗",
+                info: "blah blah blah blah blah blah blah blah blah"
+            }
+        }
     }
     render(){
-        return (<></>);
+        const shuffledDeck = [];
+
+        const shuffleCards = ()  => {
+            while (this.state.deck.length > 0) {
+                let index = Math.floor(Math.random() * deck.length);
+                let card = deck[index];
+                shuffledDeck.push(card);
+                deck.splice(index, 1);
+            }
+        }
+        return (<>
+        <div id="deckDisplay">
+            
+        </div>
+        <button onClick={shuffleCards}>Deal Deck</button>
+        
+        </>);
     };
 };
