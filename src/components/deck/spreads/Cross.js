d9e0db5a1c94d4b4656bf4b989d3e1970b70c1d7
Levi Winans <levieiko@gmail.com>
2021-01-05 20:20:18 -0800

    add basic spreads js

---
+++ src/components/deck/spreads/Cross.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class CrossSpread extends Component {
+    render(){
+        return(
+            <Container>
+                <div>test</div><div>test</div><div>test</div><div>test</div><div>test</div>
+            </Container>
+        );
+    };
+};
+
+const Container = styled.div`
+    display: grid | inline-grid;
+    grid-template-areas: 
+    ' . one .'
+    ' two three four'
+    ' . five . ';
+    > div {
+        &:first-of-type {
+            grid-area: one;
+        }
+        &:nth-of-type(2) {
+            grid-area: two;
+        }
+        &:nth-of-type(3) {
+            grid-area: three;
+        }
+        &:nth-of-type(4) {
+            grid-area: four;
+        }
+        &:nth-of-type(5) {
+            grid-area: five;
+        }
+    }
+`;
