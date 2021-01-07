1da38a6851f5b5e403233ac4775ac50309808441
Levi Winans <levieiko@gmail.com>
2021-01-07 10:55:09 -0800

    new file to show spreads

---
+++ src/components/spreads/Spreads.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class Spreads extends Component {
+    state = {
+
+    }
+    render(){
+        
+        return(
+            <LocalWrapper>
+
+            </LocalWrapper>
+        );
+    };
+};
+
+const LocalWrapper = styled.div`
+`;
