a2d86d596e2ceba928214e1335200603390d25ca
Levi Winans <levieiko@gmail.com>
2021-01-06 21:03:29 -0800

    add style to three spread

--- src/components/deck/spreads/Cross.js
+++ src/components/deck/spreads/Cross.js
@@ -4,14 +4,41 @@ import styled                   from 'styled-components';
 export default class CrossSpread extends Component {
     render(){
         return(
-            <Container>
+            <ContainerHand>
                 <div>test</div><div>test</div><div>test</div><div>test</div><div>test</div>
-            </Container>
+            </ContainerHand>
         );
     };
 };
 
-const Container = styled.div`
+const ContainerHand = styled.div`
+text-align: center;
+    display: inline-grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-areas: 
+    ' . one .'
+    ' two three four'
+    ' . five . ';
+    > div {
+        &:first-of-type {
+            grid-area: one;
+        }
+        &:nth-of-type(2) {
+            grid-area: two;
+        }
+        &:nth-of-type(3) {
+            grid-area: three;
+        }
+        &:nth-of-type(4) {
+            grid-area: four;
+        }
+        &:nth-of-type(5) {
+            grid-area: five;
+        }
+    }
+`;
+const ContainerSpread = styled.div`
 text-align: center;
     display: inline-grid;
     grid-template-columns: 1fr 1fr 1fr;
--- src/components/deck/spreads/Three.js
+++ src/components/deck/spreads/Three.js
@@ -13,34 +13,82 @@ export default class ThreeSpread extends Component {
             )
         })
         return(
-            <Container>
-                {showHand}
-            </Container>
+            <LocalWrapper>
+                <ContainerSpread>
+                    <section>
+                        <div></div><div></div><div></div>
+                    </section>
+                </ContainerSpread>
+                <ContainerHand>
+                    <section>
+
+                    {showHand}
+                    </section>
+                </ContainerHand>
+
+            </LocalWrapper>
         );
     };
 };
 
+const LocalWrapper = styled.div`
+    position: relative;
+    height: 40vw;
+    max-height: 252px;
+`;
+
 const Container = styled.div`
     text-align: center;
-    max-width: 600px;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr;
-    grid-template-areas: ' one two three ';
+    width: 100vw;
+    > section {
+        margin: 0 auto;
+        width: 100vw;
+        max-width: 600px;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr;
+        grid-template-areas: ' one two three ';
+        > div {
+            &:first-of-type {
+                grid-area: one;
+            }
+            &:nth-of-type(2) {
+                grid-area: two;
+            }
+            &:nth-of-type(3) {
+                grid-area: three;
+            }
+        }
+    }
+`;
+const ContainerHand = styled(Container)`
+    position: absolute;
+    top: 0;
+    > section {
+        > div {
+
+            > img {
+                max-width: 80%;
+            }
+        }
+    }
+`;
+const ContainerSpread = styled(Container)`
+    position: relative;
+    > section {
     > div {
-        background-image: url(./deck/back.png);
-        &:first-of-type {
-            grid-area: one;
-        }
-        &:nth-of-type(2) {
-            grid-area: two;
-        }
-        &:nth-of-type(3) {
-            grid-area: three;
-        }
+        height: 250px;
+        margin-left: 20px;
+        background-color: blue;
+        border: solid black 1px;
+        width: 158px;
+        background-image: url(./deck/back.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
         > img {
             max-width: 80%;
         }
     }
+}
 `;
