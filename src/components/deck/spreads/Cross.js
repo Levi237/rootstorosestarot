6a638b3c6b2c2a7a10acb75cf9177318cc6997a8
Levi Winans <levieiko@gmail.com>
2021-01-07 01:54:27 -0800

    add spread from three start

--- src/components/deck/spreads/Cross.js
+++ src/components/deck/spreads/Cross.js
@@ -3,65 +3,94 @@ import styled                   from 'styled-components';
 
 export default class CrossSpread extends Component {
     render(){
+        const { hand } = this.props;
+
+        const showHand = hand.map((h, k) => {
+            return (
+                <div key={k}>
+                    <img src={`./deck/${h.number}.jpg`}/>
+                </div>
+            )
+        })
         return(
-            <ContainerHand>
-                <div>test</div><div>test</div><div>test</div><div>test</div><div>test</div>
-            </ContainerHand>
+            <LocalWrapper>
+                <ContainerSpread>
+                    <section>
+                        <div>
+                            <img src="./deck/back-bw.jpg"/>
+                        </div>
+                        <div>
+                            <img src="./deck/back-bw.jpg"/>
+                        </div>
+                        <div>
+                            <img src="./deck/back-bw.jpg"/>
+                        </div>
+                        <div>
+                            <img src="./deck/back-bw.jpg"/>
+                        </div>
+                        <div>
+                            <img src="./deck/back-bw.jpg"/>
+                        </div>
+                    </section>
+                </ContainerSpread>
+                <ContainerHand>
+                    <section>
+
+                    {showHand}
+                    </section>
+                </ContainerHand>
+
+            </LocalWrapper>
         );
     };
 };
 
-const ContainerHand = styled.div`
-text-align: center;
-    display: inline-grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
-    grid-template-areas: 
-    ' . one .'
-    ' two three four'
-    ' . five . ';
-    > div {
-        &:first-of-type {
-            grid-area: one;
-        }
-        &:nth-of-type(2) {
-            grid-area: two;
-        }
-        &:nth-of-type(3) {
-            grid-area: three;
-        }
-        &:nth-of-type(4) {
-            grid-area: four;
-        }
-        &:nth-of-type(5) {
-            grid-area: five;
+const LocalWrapper = styled.div`
+    position: relative;
+    height: 120vw;
+    max-height: 800px;
+`;
+
+const Container = styled.div`
+    text-align: center;
+    width: 100vw;
+    > section {
+        margin: 0 auto;
+        width: 100vw;
+        max-width: 600px;
+        display: grid;
+        grid-template-columns: 5% 30% 30% 30% 5%;
+        grid-template-rows: auto;
+        grid-template-areas: 
+            ' . . one . . '
+            ' . two three four . '
+            ' . . five . . ';
+        > div {
+            &:first-of-type {
+                grid-area: one;
+            }
+            &:nth-of-type(2) {
+                grid-area: two;
+            }
+            &:nth-of-type(3) {
+                grid-area: three;
+            }
+            &:nth-of-type(4) {
+                grid-area: four;
+            }
+            &:nth-of-type(5) {
+                grid-area: five;
+            }
+            > img {
+                max-width: 80%;
+            }
         }
     }
 `;
-const ContainerSpread = styled.div`
-text-align: center;
-    display: inline-grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
-    grid-template-areas: 
-    ' . one .'
-    ' two three four'
-    ' . five . ';
-    > div {
-        &:first-of-type {
-            grid-area: one;
-        }
-        &:nth-of-type(2) {
-            grid-area: two;
-        }
-        &:nth-of-type(3) {
-            grid-area: three;
-        }
-        &:nth-of-type(4) {
-            grid-area: four;
-        }
-        &:nth-of-type(5) {
-            grid-area: five;
-        }
-    }
+const ContainerHand = styled(Container)`
+    position: absolute;
+    top: 0;
+`;
+const ContainerSpread = styled(Container)`
+    position: relative;
 `;
