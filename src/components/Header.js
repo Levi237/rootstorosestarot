582bdb86e07310e5ad1fd2e146f03e23b2bcdaa2
Levi Winans <levieiko@gmail.com>
2021-01-07 15:45:19 -0800

    create header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled               from 'styled-components';
+
+export default class Header extends Component {
+    render(){
+        return(
+            <LocalWrapper>
+                <h1>Hello Angie :)</h1>
+            </LocalWrapper>
+        )
+    }
+}
+
+const LocalWrapper = styled.header`
+height: 80px;
+background-color: black;
+color: white;
+`;
