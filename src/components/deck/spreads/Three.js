c47e27da7603f4d19f9c872aadbbf5e757b0218b
Levi Winans <levieiko@gmail.com>
2021-01-06 20:26:36 -0800

    merge

---
+++ src/components/deck/spreads/Three.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class ThreeSpread extends Component {
+    render(){
+        const { hand } = this.props;
+
+        const showHand = hand.map((h, k) => {
+            return (
+                <div key={k}>
+                    <img src={`./deck/${h.number}.jpg`}/>
+                </div>
+            )
+        })
+        return(
+            <Container>
+                {showHand}
+            </Container>
+        );
+    };
+};
+
+const Container = styled.div`
+    text-align: center;
+    max-width: 600px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: ' one two three ';
+    > div {
+        background-image: url(./deck/back.png);
+        &:first-of-type {
+            grid-area: one;
+        }
+        &:nth-of-type(2) {
+            grid-area: two;
+        }
+        &:nth-of-type(3) {
+            grid-area: three;
+        }
+        > img {
+            max-width: 80%;
+        }
+    }
+`;
