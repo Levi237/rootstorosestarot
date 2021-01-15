8db24639e409759842cc074e2dc66075da2e78ce
Levi Winans <levieiko@gmail.com>
2021-01-14 18:27:56 -0800

    raise deck

--- src/App.js
+++ src/App.js
@@ -404,11 +404,11 @@ export default class App extends Component {
     return (
       <AppContainer> 
         <Header/>
+          <Deck selectSpread={selectSpread} deck={deck} hand={hand} selectCard={this.selectCard} shuffleThis={this.shuffleThis} shuffle={shuffle}/>
           <Spreads spreads={spreads} selectSpread={this.selectSpread}/>
           { selectSpread.name === "three" && <SimpleSpread hand={hand}/> }
           { selectSpread.name === "cross" && <CrossSpread hand={hand}/> }
           {/* { selectSpread.name === "celtic cross" && <CelticCrossSpread hand={hand}/> } */}
-          <Deck selectSpread={selectSpread} deck={deck} hand={hand} selectCard={this.selectCard} shuffleThis={this.shuffleThis} shuffle={shuffle}/>
       </AppContainer>
     );
   };
