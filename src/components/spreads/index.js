547926b4e17742751315d62e3dbe8cc7e944b80f
Levi Winans <levieiko@gmail.com>
2021-05-25 13:15:58 -0700

    add style, move function to app

--- src/components/spreads/index.js
+++ src/components/spreads/index.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 export default class Spreads extends Component {
-    componentDidUpdate(){
-        this.animateDeck();
-    };
-    animateDeck(){   
-        let getCard = document.getElementsByClassName('dealtCard');
-        for (let i = 0; i < getCard.length; i++) {
-            setTimeout(() => {
-                setTimeout(() => {
-                    getCard[i].classList.remove('shuffleDeck');
-                }, i*20);
-            }, 0);
-        };
-    };
+    // componentDidUpdate(){
+    //     this.animateDeck();
+    // };
+    // animateDeck(){   
+    //     let getCard = document.getElementsByClassName('dealtCard');
+    //     for (let i = 0; i < getCard.length; i++) {
+    //         setTimeout(() => {
+    //             setTimeout(() => {
+    //                 getCard[i].classList.remove('shuffleDeck');
+    //             }, i*20);
+    //         }, 0);
+    //     };
+    // };
     render(){
         const { spreads, selectSpread } = this.props
                 const spreadOptions = spreads.map((s, key) => {
@@ -35,16 +35,19 @@ export default class Spreads extends Component {
 
 const LocalWrapper = styled.section`
     text-align: center;
+    max-width: 640px;
+    width: 100vw;
+    margin: 0 auto;
     > button {
         color: white;
         text-shadow: 2px 2px 5px rgba(0,0,0,.75);
         display: inline-block;
         width: 30vw;
-        max-width: 200px;
+        max-width: 197px;
         height: 30vw;
-        max-height: 200px;
+        max-height: 197px;
         border: 2px solid white;
-        margin: 1vw;
+        margin: clamp(.1vw, 1vw, 8px);
         vertical-align: top;
         background-color: transparent;
         > h2 {
@@ -57,7 +60,7 @@ const LocalWrapper = styled.section`
             background-repeat: no-repeat;
         }
     }
-    @media only screen and (max-width: 900px) {
+    @media only screen and (max-width: 640px) {
         > button {
             width: calc(45vw - 4px);
             max-width: calc(45vw - 4px);
